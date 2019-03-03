8583aa2a9a7ba0ddcec9588003705107f17d2d2d
Justin Piotroski <justin.piotroski@gmail.com>
2019-03-02 22:20:41 -0800

    Updates to docs

--- lib/ulidtools.rb
+++ lib/ulidtools.rb
@@ -8,10 +8,12 @@ module ULIDTools
     ULID.new
   end
 
+  # Takes in the +time+ and creates a new ULID from it
   def self.at_time(time)
   	ULID.new(time: time)
   end
 
+  # Converts a +uuid+ into a ULID
   def self.parse_uuid(uuid)
   	ULID.new(raw: uuid.split('-').pack(ULID::UUID_PACKING))
   end
